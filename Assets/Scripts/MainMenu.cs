80bba87a6309640f0fe9622d2f659abf9cf62cd6
korojegzerbchik-commits <korojegzerbchik@gmail.com>
2026-07-23 17:52:40 +0200

    Доделал то что не доделал в прошлом комите

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro; // 1. Подключаем TextMeshPro для работы с TMP_Dropdown
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string _playSceneName = "SampleScene";
     [SerializeField] private GameObject _settingsPanel;
+    [SerializeField] private TMP_Dropdown _fpsDropdown;
+
+    private void Start()
+    {
+        int savedIndex = PlayerPrefs.GetInt("SavedFPSIndex", 3);
+
+        ChangeFrameRate(savedIndex);
+        
+        if (_fpsDropdown != null)
+        {
+            _fpsDropdown.value = savedIndex;
+            _fpsDropdown.RefreshShownValue();
+        }
+    }
+
+    public void ChangeFrameRate(int index)
+    {
+        QualitySettings.vSyncCount = 0;
+        
+        switch (index)
+        {
+            case 0: Application.targetFrameRate = 60; break;
+            case 1: Application.targetFrameRate = 144; break;
+            case 2: Application.targetFrameRate = 240; break;
+            case 3: Application.targetFrameRate = -1; break;
+        }
+        
+        PlayerPrefs.SetInt("SavedFPSIndex", index);
+        PlayerPrefs.Save();
+    }
 
     public void OnPlayClick()
     {
